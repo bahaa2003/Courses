abd60c03cb0d1ac8ca8c9c817cbe45a2fe77cb40
bahaa2003 <77056859+bahaa2003@users.noreply.github.com>
2025-04-25 11:34:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 
 app.get('/favicon.png', (req, res) => res.status(204).end());
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 app.use('/api/courses', courseRoutes);
 app.use(globalErrorHandler);
 
